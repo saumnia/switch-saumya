9361df82c9796d9793a03abe027ceb7c3d4510a7
Graphic <graphic@SOD-SJH-605-03M.local>
2016-02-02 16:36:48 -0500

    switch assignmnet

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
+$('button').on('click', function(){
+	$('.on').toggleClass('off');
 
+	if ($('button').hasClass('off')) {
+		$('body').addClass('dark');
+		$('.status').text("Hey, who turned off the lights?");
+	}
 
+	else {
+		$('body').removeClass('dark');
+		$('.status').text("It's so bright in here!");
+	}
+});
